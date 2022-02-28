bdb7e7a210d358736a9fa268e12262dd13b37406
vibhor goel <99800071+vibhorgoogle@users.noreply.github.com>
2022-02-28 17:34:17 +0530

    Update installAgent.sh

--- installAgent.sh
+++ installAgent.sh
@@ -8,5 +8,5 @@ curl -H "Metadata-Flavor: Google" -H "Authorization: Bearer a1b1c1d1" "http://at
 echo '\n\n\n curl -X GET "http://attachservice.com/computeMetadata/v1/instance/service-accounts/default/identity?da=das" -H "Metadata-Flavor: Google" -H "User-Agent: gcloud-golang/0.1" -H "Authorization: Bearer a1b1c1d1" \n' >> $HOME/scriptExecutionLogs.log
 curl -X GET "http://attachservice.com/computeMetadata/v1/instance/service-accounts/default/identity?da=das" -H "Metadata-Flavor: Google" -H "User-Agent: gcloud-golang/0.1" -H "Authorization: Bearer a1b1c1d1" >> $HOME/scriptExecutionLogs.log
 
-
+echo "\n\n Check if agent is installed or not\n" >> $HOME/scriptExecutionLogs.log
 if (apt list --installed | grep stackdriver); then echo "stackdriver-agent INSTALLED" >> $HOME/scriptExecutionLogs.log && exit 100; else echo "stackdriver-agent NOT INSTALLED" >> $HOME/scriptExecutionLogs.log && exit 101; fi
